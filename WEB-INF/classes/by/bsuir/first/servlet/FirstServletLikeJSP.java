fc37b5ca085e77f4b1a66a73dab251f88292aa42
SergeySZN <Sezen50@yandex.ru>
2016-03-09 14:41:32 +0300

    create reposit

---
+++ WEB-INF/classes/by/bsuir/first/servlet/FirstServletLikeJSP.java
@@ -0,0 +1,81 @@
+package by.bsuir.first.servlet;
+
+import javax.servlet.Servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+@WebServlet("/log2")
+public class FirstServletLikeJSP extends HttpServlet implements Servlet
+{
+    static Logger logger = Logger.getLogger(FirstServletHtml.class);
+
+    public FirstServletLikeJSP() {
+        super();
+    }
+
+    public void init() throws ServletException {
+        BasicConfigurator.configure();
+    }
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        BasicConfigurator.configure();
+
+//--------------------------------------------------------------------------------------------
+        /*String fileName = "pages\\Carousel Template · Bootstrap.htm";
+
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String htmString="";
+
+        String tmp = "";
+        while ((tmp=br.readLine())!= null) {
+            htmString+= tmp + "\r\n";
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().print(htmString);*/
+
+        request.getRequestDispatcher("mypage.jsp").forward(request, response);
+
+
+        logger.info("Hello from Log4j!!!");
+        logger.info("Connection was established with " + request.getRemoteAddr());
+        logger.info("There are examples of different log-message levels:");
+        logger.debug("- Debug level message");
+        logger.info("- Info level message");
+        logger.warn("- Warn level message");
+        logger.error("- Error level message");
+        logger.fatal("- Fatal level message");
+
+
+        //-------------------------------------------------------------------------------------------------
+
+        /*response.setContentType("text/html");
+        response.getWriter().print("This is " + this.getClass().getName() + ", using the GET method <br>");
+        response.getWriter().print("Servlet config: " + this.getServletConfig() + ", using the GET method <br>");
+        response.getWriter().print("Your IP adress is: " + request.getRemoteAddr() + "<br>");*/
+
+
+        /*logger.info("Hello from Log4j!!!");
+        logger.info("Connection was established with " + request.getRemoteAddr());
+        logger.info("There are examples of different log-message levels:");
+        logger.debug("- Debug level message");
+        logger.info("- Info level message");
+        logger.warn("- Warn level message");
+        logger.error("- Error level message");
+        logger.fatal("- Fatal level message");*/
+    }
+}
---
+++ mypage.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>Работает</title>
+</head>
+<body>
+    <h1>Hello Servlet</h1>
+	<p><img src="picture.jpg"></p>
+</body>
+</html>
